f9acd4abfd8bb78b629125ec77be07cb3a6bf3d8
Carlo Amorelli Caputo <zed@z.9hells.org>
2008-05-05 22:36:24 -0300

    put M_PI_2 define on a broader context

--- src/game.c
+++ src/game.c
@@ -26,14 +26,6 @@
 #include "vec.h"
 #include "game.h"
 
-//ifdef _WIN32 // XXX no M_PI on mingw???
-#ifndef M_PI
-# define M_PI		3.14159265358979323846	/* pi */
-#endif
-#ifndef M_PI_2
-# define M_PI_2		1.57079632679489661923	/* pi/2 */
-#endif
-
 float cave_len (Cave *cave)
 {
 	int head = cave->i;
